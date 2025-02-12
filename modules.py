d618aec3968af18e51ed48f2e112792fb71e259d
Makoo <andaya.mark.roel.03122003@gmail.com>
2025-02-12 15:37:25 +0800

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -10,10 +12,10 @@ class loginpage_modules:
     def __init__(self, driver):
         self.driver = driver
         
-    def login(self):
-        self.driver.find_element(By.NAME, "username").send_keys("Admin")
-        self.driver.find_element(By.NAME, "password").send_keys("admin123")
-        screenshot_path = self.take_screenshot(driver , folder_path = "ORANGEHRM-ScriptTest\Screenshots", file_name="001 Login.png")
+    def login(self, username="Admin", password="admin123"):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
+        # screenshot_path = self.take_screenshot(driver , folder_path = "ORANGEHRM-ScriptTest\Screenshots", file_name="001 Login.png")
         self.driver.find_element(By.CLASS_NAME, "oxd-button--main").click()
         time.sleep(10)   
 
@@ -48,3 +50,35 @@ class clickmodule:
     def click_performance(self):
         self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[7]/a/span').click()
         time.sleep(5)
+        
+class func:
+    
+    def __init__(self, driver):
+        self.driver = driver
+        
+    def addEmployee(self):
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button').click()
+        time.sleep(10)
+
+        self.driver.find_element(By.NAME, "firstName").send_keys("fname")
+        self.driver.find_element(By.NAME, "middleName").send_keys("mname")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[2]/input').send_keys("lname")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input').send_keys("121222")
+
+        elem = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".oxd-switch-input.oxd-switch-input--active.--label-right"))
+        )
+        self.driver.execute_script("arguments[0].click();", elem)
+
+        time.sleep(10)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[3]/div/div[1]/div/div[2]/input').send_keys("username")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[1]/div/div[2]/input').send_keys("password123")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[2]/div/div[2]/input').send_keys("password123")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]').click()
+        
+        
